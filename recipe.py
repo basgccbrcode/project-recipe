cb061c4b69adfbaadc09ca0dfd137ed0932c663e
TheFreak <129125060+Ratnips@users.noreply.github.com>
2026-05-20 09:41:01 +1000

    i wrote some code for the time and temperature

--- recipe.py
+++ recipe.py
@@ -1,24 +1,32 @@
 class Recipe:
-    def __init__(self, name, ingredients, instructions, category, rating):
+    def __init__(self, name, ingredients, instructions, time, temp, category, rating):
         self.name = name
         self.ingredients = ingredients
         self.instructions = instructions
         self.category = category
         self.rating = rating
-
-#----------------------
+        self.time = time
+        self.temp = temp
+#Michael's Changes feel free to change back if needed
 
 def receipe_name(self, name):
     self.name = name
 
+def time_num(self, time):
+     self.time = time
+
+def temp_num(self, temp):
+    self.temp = temp
+
 def receipe_ingredients(self, ingredients):
     print(f"Recipe: {self.name}")
+    print(f"Time: {self.time}")
     print("Ingredients:")
     for ingredient in self.ingredients:
         print(f"- {ingredient}")
     self.ingredients += ingredients
 
-    self.instructions = "preheat oven to temperature (what is that in celsius?), mix ingredients, bake for time minutes"
+    self.instructions = print(f"preheat oven to {temperature_num}, mix ingredients, bake for {time_num} minutes")
 
     # Homework: to be continued...
 #recipe_list[Recipe(field, field, field), Recipe(field, field, field)]

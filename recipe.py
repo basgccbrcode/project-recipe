af668df1d29719842cde91ca62c2b50a90f4737f
sebastian carr <0195659@schoolsnet.act.edu.au>
2026-05-19 14:54:23 +1000

    add python file draft

---
+++ recipe.py
@@ -0,0 +1,23 @@
+class Recipe:
+    def __init__(self, name, ingredients, instructions, category, rating):
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.category = category
+        self.rating = rating
+
+#----------------------
+
+def receipe_name(self, name):
+    self.name = name
+
+def receipe_ingredients(self, ingredients):
+    print(f"Recipe: {self.name}")
+    print("Ingredients:")
+    for ingredient in self.ingredients:
+        print(f"- {ingredient}")
+    self.ingredients += ingredients
+
+    self.instructions = "preheat oven to temperature (what is that in celsius?), mix ingredients, bake for time minutes"
+
+    # Homework: to be continued...
